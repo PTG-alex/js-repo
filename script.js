92844a6199d313f59331fe7d0f28cadd805f6e52
PTG-alex <alex@ptglab.com>
2024-12-04 15:13:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+<script>
+    const campaignData = [
+    { image: 'https://www.ptglab.com/media/zwvp2x3y/dec-14.png', text: 'Day 1: Special Offer on Gifts!' },
+    { image: 'https://www.ptglab.com/media/vtwd1roq/dec-15.png', text: 'Day 2: Save on all Orders!' },
+    { image: 'https://www.ptglab.com/media/l0ydg4te/dec-16.png', text: 'Day 3: Free Shipping Today!' },
+    { image: 'https://www.ptglab.com/media/3mynkb3h/dec-17.png', text: 'Day 4: Exclusive Christmas Deals!' },
+    { image: 'https://www.ptglab.com/media/yzlpk3ln/dec-18.png', text: 'Day 5: Buy 1 Get 1 Free!' },
+    { image: 'day6.jpg', text: 'Day 6: Discounts on Bestsellers!' },
+    { image: 'day7.jpg', text: 'Day 7: Gifts for Everyone!' },
+    { image: 'day8.jpg', text: 'Day 8: Shop More, Save More!' },
+    { image: 'day9.jpg', text: 'Day 9: Limited Time Christmas Sale!' },
+    { image: 'day10.jpg', text: 'Day 10: Early Bird Specials!' },
+    { image: 'day11.jpg', text: 'Day 11: Countdown to Christmas!' },
+    { image: 'day12.jpg', text: 'Day 12: Final Christmas Offer!' }
+];
+
  // Get current Pacific Time
  const now = new Date();
     const pacificOffset = new Date().getTimezoneOffset() + 480; // Pacific Time is UTC-8
